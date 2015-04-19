bbf6ab8b2a81aa92d559a5c104528256da89ba7e
kemintha <kemintha@gmail.com>
2015-04-19 21:40:16 +0200

    Header and declaration files added with outlines

---
+++ Time.cpp
@@ -0,0 +1,5 @@
+//Declaration of methods for class time
+#include "Time.hpp"
+#include <iostream>
+#include <string>
+using namespace std;
---
+++ Time.hpp
@@ -0,0 +1,13 @@
+//Header file for class Time
+
+#pragma once
+#ifndef TIME_H
+#define TIME_H
+using namespace std;
+class Time
+{
+private:
+public:
+};
+
+#endif
