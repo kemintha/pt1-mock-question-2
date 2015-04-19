2f118cd4e97566cc3f3ce51bde01cbf31e18acc3
kemintha <kemintha@gmail.com>
2015-04-19 22:16:42 +0200

    2.1.

--- Time.cpp
+++ Time.cpp
@@ -3,3 +3,28 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+Time::Time(int a, int b)
+{
+	hour = a;
+	min = b;
+}
+Time::Time()
+{
+	hour = 0;
+	min = 0;
+}
+Time::~Time()
+{
+}
+void Time::set(int a, int b)
+{
+	hour = a;
+	min = b;
+}
+void Time::get(int &a, int &b)
+{
+	a = hour;
+	b = min;
+}
+
--- Time.hpp
+++ Time.hpp
@@ -7,7 +7,17 @@ using namespace std;
 class Time
 {
 private:
+	int hour;
+	int min;
 public:
+	Time(int, int);
+	Time();
+	~Time();
+	void set(int, int);
+	void get(int&, int&);
+
+
+
 };
 
 #endif
